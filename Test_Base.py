689874fe234d98973c534dcd48a95aa61ce17454
Dilson Carneiro <39513861+dilsoncarneiro@users.noreply.github.com>
2025-08-13 10:45:30 +1000

    Update Test_Base.py

--- Test_Base.py
+++ Test_Base.py
@@ -1,6 +1,21 @@
 import os
 import pandas as pd
 
+# from selenium import webdriver
+# from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.options import Options
+
+# options = Options()
+# options.add_argument("--headless")
+# options.add_argument("--no-sandbox")
+# options.add_argument("--disable-dev-shm-usage")
+
+# driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+
+
+
 import urllib3
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
@@ -147,4 +162,5 @@ class TestBase:
     # @staticmethod
     def teardown(driver):
         driver.close()
+
         driver.quit()
